36053f0f4e066a39bde2098770231846533ee6a0
avnigodhala <139811126+avnigodhala@users.noreply.github.com>
2023-07-17 12:33:06 -0700

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,17 @@
+var sprite
+
+function setup() {
+  createCanvas(400,400);
+sprite=createSprite(200,200,50,50);
+sprite.shapeColor="blue"
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+}
+
+
+
+
